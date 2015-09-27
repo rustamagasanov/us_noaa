252bb2db9344a55d7319183afd15e7bb8c72649d
Rustam Gasanov <rustamagasanov@gmail.com>
2015-09-27 18:40:26 -0400

    Implemented simple fetch function

--- lib/us_noaa/xml_fetcher.ex
+++ lib/us_noaa/xml_fetcher.ex
@@ -1,7 +1,7 @@
 defmodule UsNoaa.XMLFetcher do
-  @site = "http://w1.weather.gov/xml/current_obs/"
+  @site "http://w1.weather.gov/xml/current_obs/"
 
   def fetch(state_id) do
-    # HTTPoison.get
+    HTTPoison.get("#{@site}/#{state_id}.xml")
   end
 end
