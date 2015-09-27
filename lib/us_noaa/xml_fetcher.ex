c092eb77fa9daa5eb2a378923d2a3e8f66dc0f55
Rustam Gasanov <rustamagasanov@gmail.com>
2015-09-27 18:45:33 -0400

    Inspecting XML for now

--- lib/us_noaa/xml_fetcher.ex
+++ lib/us_noaa/xml_fetcher.ex
@@ -2,6 +2,6 @@ defmodule UsNoaa.XMLFetcher do
   @site "http://w1.weather.gov/xml/current_obs/"
 
   def fetch(state_id) do
-    HTTPoison.get("#{@site}/#{state_id}.xml")
+    IO.inspect HTTPoison.get("#{@site}/#{state_id}.xml")
   end
 end
