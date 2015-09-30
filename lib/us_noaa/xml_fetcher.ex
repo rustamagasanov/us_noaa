bef7b95e0ea88574c8b5ebea16f1667cf9e8ca75
Rustam Gasanov <rustamagasanov@gmail.com>
2015-09-30 23:57:13 +0200

    Started XML parsing

--- lib/us_noaa/xml_fetcher.ex
+++ lib/us_noaa/xml_fetcher.ex
@@ -1,7 +1,18 @@
 defmodule UsNoaa.XMLFetcher do
+  require Logger
+
   @site Application.get_env(:us_noaa, :site)
 
   def fetch(state_id) do
     HTTPoison.get("#{@site}/#{state_id}.xml")
+      |> handle_response
+  end
+
+  defp handle_response({ :ok, %HTTPoison.Response{ status_code: 200, body: body } }) do
+    body
+  end
+
+  defp handle_response({ :error, %HTTPoison.Error{ reason: reason } }) do
+    Logger.error "Error #{reason} returned"
   end
 end
--- lib/us_noaa/xml_parser.ex
+++ lib/us_noaa/xml_parser.ex
@@ -1,5 +1,6 @@
 defmodule UsNoaa.XMLParser do
   def parse(xml) do
     IO.inspect xml
+    :xmerl_scan.string(String.to_char_list(xml))
   end
 end
