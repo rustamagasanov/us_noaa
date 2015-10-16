f3a1439ffa051b990af6a774667c42514b42d74e
Rustam Gasanov <rustamagasanov@gmail.com>
2015-10-15 20:41:19 -0400

    defrecord example with xmerl

--- lib/us_noaa/xml_parser.ex
+++ lib/us_noaa/xml_parser.ex
@@ -1,3 +1,6 @@
+# defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+# defrecord :xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
+
 defmodule UsNoaa.XMLParser do
   def parse(xml) do
     xml
@@ -9,7 +12,12 @@ defmodule UsNoaa.XMLParser do
     :xmerl_scan.string(String.to_char_list(xml))
   end
 
-  defp get_data(scanned_xml) do
-    IO.inspect scanned_xml
+  defp get_data({ xml, _rest }) do
+    # IO.inspect xml
+    [credit_element]  = :xmerl_xpath.string('/current_observation/credit', xml)
+    [credit_text] = credit_element.content
+    credit = credit_text.value
+
+    IO.inspect credit
   end
 end
